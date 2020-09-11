55500b983ab0fe7301f948a808c6bb79bb7e5758
mcj010 <329392494@qq.com>
2020-09-11 14:28:51 +0800

    有N张火车票，每张票都有一个编号，同时有10个窗口对外售票，请写一个模拟程序。 Vector to Queue.

---
+++ src/main/java/com/mcj010/c_024_FromVectorToQueue/TicketSeller4.java
@@ -0,0 +1,27 @@
+package com.mcj010.c_024_FromVectorToQueue;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class TicketSeller4 {
+    static Queue<String> tickets = new ConcurrentLinkedQueue<>();
+
+    static {
+        for (int i = 0; i < 1000; i++) {
+            tickets.add("票编号：" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                while (true) {
+                    String s = tickets.poll();//从 Queue删除并返回
+                    if (s == null) break;
+                    else System.out.println("销售了--" + s);
+                }
+            }).start();
+        }
+    }
+}
