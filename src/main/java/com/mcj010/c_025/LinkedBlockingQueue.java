0e1d8515846960b31e572ecef36875c5c43e6ffc
mcj010 <329392494@qq.com>
2020-09-12 10:37:40 +0800

    未写完

---
+++ src/main/java/com/mcj010/c_025/LinkedBlockingQueue.java
@@ -0,0 +1,12 @@
+package com.mcj010.c_025;
+
+import java.util.Random;
+
+public class LinkedBlockingQueue {
+
+    static Random r = new Random();
+
+    public static void main(String[] args) {
+
+    }
+}
